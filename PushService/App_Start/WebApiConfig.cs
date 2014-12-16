2106f4cc60defd5a081e35a35b1fb1435b694c18
Jongho Lee <mail@jongholee.kr>
2014-12-16 23:36:32 +0900

    change

--- PushService/App_Start/WebApiConfig.cs
+++ PushService/App_Start/WebApiConfig.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Ninject;
+using Ninject.Activation;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Dependencies;
 
 namespace PushService
 {
@@ -10,6 +15,7 @@ namespace PushService
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.DependencyResolver = new NinjectResolver(NinjectConfig.CreateKernel());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -21,4 +27,67 @@ namespace PushService
             );
         }
     }
+
+    public class NinjectConfig
+    {
+        public static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            try
+            {
+                kernel.Bind<PushSharp.PushBroker>().To<PushSharp.PushBroker>();
+                return kernel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class NinjectScope : IDependencyScope
+    {
+        protected IResolutionRoot resolutionRoot;
+
+        public NinjectScope(IResolutionRoot kernel)
+        {
+            resolutionRoot = kernel;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            IRequest request = resolutionRoot.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return resolutionRoot.Resolve(request).SingleOrDefault();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            IRequest request = resolutionRoot.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return resolutionRoot.Resolve(request).ToList();
+        }
+
+        public void Dispose()
+        {
+            IDisposable disposable = (IDisposable)resolutionRoot;
+            if (disposable != null) disposable.Dispose();
+            resolutionRoot = null;
+        }
+    }
+
+    public class NinjectResolver : NinjectScope, IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectResolver(IKernel kernel) : base(kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectScope(_kernel.BeginBlock());
+        }
+    }
 }
+
