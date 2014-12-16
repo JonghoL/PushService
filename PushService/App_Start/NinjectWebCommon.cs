be3c51b7707390334118965a3571700c50521e29
Jongho Lee <mail@jongholee.kr>
2014-12-17 01:41:52 +0900

    add redis

--- PushService/App_Start/NinjectWebCommon.cs
+++ PushService/App_Start/NinjectWebCommon.cs
@@ -45,6 +45,7 @@ namespace PushService.App_Start
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
                 kernel.Bind<PushSharp.PushBroker>().To<PushSharp.PushBroker>();
+                kernel.Bind<Redis>().To<Redis>();
 
                 RegisterServices(kernel);
                 return kernel;
---
+++ PushService/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace PushService.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        Redis Redis { get; set; }
+        public RegisterController(Redis r)
+        {
+            this.Redis = r;
+            var redisUrl = ConfigurationManager.AppSettings["redistogo"];
+            var connectionUri = new Uri(redisUrl);
+            var password = connectionUri.UserInfo.Split(':').LastOrDefault();
+
+            this.Redis.Host = connectionUri.Host;
+            this.Redis.Port = connectionUri.Port;
+            this.Redis.Password = password;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return this.Redis.Keys;
+        }
+
+        public string Get(string id)
+        {
+            return this.Redis.GetString(id);
+        }
+
+        public void Post(FormDataCollection form)
+        {
+            var key = form["key"];
+            var value = form["value"];
+            this.Redis.Set(key, value);
+        }
+    }
+}
