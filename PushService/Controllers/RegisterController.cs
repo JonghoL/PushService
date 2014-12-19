38fc89caf56f05b607be67f4c4e64d4bf41f3f25
Jongho Lee <mail@jongholee.kr>
2014-12-19 21:53:46 +0900

    apply servicestack.redis

--- PushService/Controllers/RegisterController.cs
+++ PushService/Controllers/RegisterController.cs
@@ -7,90 +7,101 @@ using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Text;
 using System.Web.Http;
+using ServiceStack.Redis;
 
 namespace PushService.Controllers
 {
     public class RegisterController : ApiController
     {
-        Redis Redis { get; set; }
-        public RegisterController(Redis r)
+        private readonly string redis_host;
+        private readonly int redis_port;
+        private readonly string redis_password;
+        public RegisterController()
         {
-            this.Redis = r;
             var redisUrl = ConfigurationManager.AppSettings["redistogo"];
             var connectionUri = new Uri(redisUrl);
             var password = connectionUri.UserInfo.Split(':').LastOrDefault();
 
-            this.Redis.Host = connectionUri.Host;
-            this.Redis.Port = connectionUri.Port;
-            this.Redis.Password = password;
+            this.redis_host = connectionUri.Host;
+            this.redis_port = connectionUri.Port;
+            this.redis_password = password;
         }
 
         public IEnumerable<string> Get()
         {
-            return this.Redis.Keys;
+            using (var redis = new RedisClient(this.redis_host, this.redis_port, this.redis_password))
+            {
+                return redis.Keys("*").Select(b => Encoding.UTF8.GetString(b));
+            }
         }
 
         public string Get(string id)
         {
-            if (id.Length == 64)
+            using (var redis = new RedisClient(this.redis_host, this.redis_port, this.redis_password))
             {
-                var prevUserData = this.Redis.Get(id);
-                if (prevUserData == null)
+                if (id.Length == 64)
                 {
-                    return "Empty";
+                    var prevUserData = redis.Get(id);
+                    if (prevUserData == null)
+                    {
+                        return "Empty";
+                    }
+                    return Encoding.UTF8.GetString(prevUserData);
                 }
-                return Encoding.UTF8.GetString(prevUserData);
-            }
-            else
-            {
-                var deviceTokens = this.Redis.ListRange(id, 0, -1);
-                var deviceTokenList = new List<string>(deviceTokens.Length);
-                for (int i = 0; i < deviceTokens.Length; i++)
+                else
                 {
-                    var curDeviceToken = deviceTokens[i];
-                    deviceTokenList.Add(Encoding.UTF8.GetString(curDeviceToken));
-                }
-                var sb = new StringBuilder();
-                foreach (var dt in deviceTokenList)
-                {
-                    sb.AppendLine(dt);
-                }
+                    var deviceTokens = redis.LRange(id, 0, -1);
+                    var deviceTokenList = new List<string>(deviceTokens.Length);
+                    for (int i = 0; i < deviceTokens.Length; i++)
+                    {
+                        var curDeviceToken = deviceTokens[i];
+                        deviceTokenList.Add(Encoding.UTF8.GetString(curDeviceToken));
+                    }
+                    var sb = new StringBuilder();
+                    foreach (var dt in deviceTokenList)
+                    {
+                        sb.AppendLine(dt);
+                    }
 
-                return sb.ToString();
+                    return sb.ToString();
+                }
             }
         }
 
         public void Post(FormDataCollection form)
         {
-            var userId = form["key"]; 
-            var deviceToken = form["value"];
+            using (var redis = new RedisClient(this.redis_host, this.redis_port, this.redis_password))
+            {
+                var userId = form["key"];
+                var deviceToken = form["value"];
 
-            var prevUserData = this.Redis.Get(deviceToken);
-            if (prevUserData == null)
-            {
-                this.Redis.RightPush(userId, deviceToken); //list
-                this.Redis.Set(deviceToken, userId); //key-value
-            }
-            else
-            {
-                var prevUser = Encoding.UTF8.GetString(prevUserData);
-                var deviceTokens = this.Redis.ListRange(prevUser, 0, -1);
-                var deviceTokenList = new List<string>(deviceTokens.Length);
-                for (int i = 0; i < deviceTokens.Length; i++)
+                var prevUserData = redis.Get(deviceToken);
+                if (prevUserData == null)
                 {
-                    var curDeviceToken = deviceTokens[i];
-                    deviceTokenList.Add(Encoding.UTF8.GetString(curDeviceToken));
+                    redis.RPush(userId, deviceToken.ToUtf8Bytes()); //list
+                    redis.Set(deviceToken, userId); //key-value
                 }
-                if (prevUser != userId)
+                else
                 {
-                    if (deviceTokenList.Exists(t => t == deviceToken))
+                    var prevUser = Encoding.UTF8.GetString(prevUserData);
+                    var deviceTokens = redis.LRange(prevUser, 0, -1);
+                    var deviceTokenList = new List<string>(deviceTokens.Length);
+                    for (int i = 0; i < deviceTokens.Length; i++)
                     {
-                        this.Redis.ListRemove(prevUser, deviceToken);
+                        var curDeviceToken = deviceTokens[i];
+                        deviceTokenList.Add(Encoding.UTF8.GetString(curDeviceToken));
                     }
-                    this.Redis.RightPush(userId, deviceToken); //list
-                }
+                    if (prevUser != userId)
+                    {
+                        if (deviceTokenList.Exists(t => t == deviceToken))
+                        {
+                            redis.LRem(prevUser, 0, deviceToken.ToUtf8Bytes());
+                        }
+                        redis.RPush(userId, deviceToken.ToUtf8Bytes()); //list
+                    }
 
-                this.Redis.Set(deviceToken, userId); //update
+                    redis.Set(deviceToken, userId); //update
+                }
             }
         }
     }
---
+++ PushService/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PushService
+{
+    public static class StringExtensions
+    {
+        public static byte[] ToUtf8Bytes(this string str)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(str);
+        }
+    }
+}
